ccd34cb63f5d2557de9bfb2527aef6eb60be0556
moinkazi123 <155711429+moinkazi123@users.noreply.github.com>
2024-10-01 12:54:47 +0530

    Delete stop_application.sh

--- stop_application.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Stop any running instance of the application
-if pgrep -f "dotnet.*WebApplication2.dll" > /dev/null; then
-    pkill -f "dotnet.*WebApplication2.dll"
-    echo "Application stopped"
-else
-    echo "Application is not running"
-fi
