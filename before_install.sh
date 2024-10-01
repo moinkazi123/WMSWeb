6fb015c390d9d9cf3ef86f8997fc0bf3ad5453e5
moinkazi123 <155711429+moinkazi123@users.noreply.github.com>
2024-10-01 12:54:57 +0530

    Delete before_install.sh

--- before_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install .NET SDK if not already installed
-if ! command -v dotnet &> /dev/null; then
-    sudo rpm -Uvh https://packages.microsoft.com/config/rhel/7/packages-microsoft-prod.rpm
-    sudo yum install -y dotnet-sdk-8.0
-fi
-
-# Create application directory if it doesn't exist
-sudo mkdir -p /var/www/webapplication
