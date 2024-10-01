249e64d0912e4b3da48e12af7a90df6b4d280dd9
moinkazi123 <155711429+moinkazi123@users.noreply.github.com>
2024-10-01 12:41:15 +0530

    Delete start_application.sh

--- start_application.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Navigate to the application directory
-cd /var/www/dotnetapp
-
-# Stop any existing application running (optional)
-pkill dotnet || true
-
-# Start the application (assuming WMSCOREAPI.dll is the entry point)
-nohup dotnet WMSCOREAPI.dll > /dev/null 2>&1 &
